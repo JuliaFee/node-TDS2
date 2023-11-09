a28fcd1688a17746dc255a74662b36bd27dfc06e
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-09 14:58:22 -0300

    feat: create serv

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express"; // serve para escutar as requisições
+import { config } from "dotenv"; // serve para ler o arquivo .env
+config(); // le o arquivo .env
+
+const port = process.env.PORT || 5000; // define a porta q sera usada e substituira pela porta 5000 se algo der errado
+const app = express(); // Cria o servidor recebendo todo conteudo do express
+app.use(express.json()); // faz com que a variavel app converse em json
+
+app.get ("/", (req, res) => {
+    res.status(200).json({message: "hello world"})
+});
+
+app.listen(port, () =>{
+    console.log(`server running on port http://localhost:${port}`);
+})
