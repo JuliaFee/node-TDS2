673c91a15084129acbfcaabf7865154d3cc81f86
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-09 16:59:25 -0300

    feat: server config

--- src/server.js
+++ src/server.js
@@ -7,11 +7,29 @@ const app = express(); // Cria o servidor recebendo todo conteudo do express
 app.use(express.json()); // faz com que a variavel app converse em json
 
 app.get ("/", (req, res) => {
-    res.status(200).json({message: "hello get"})
+    const nome = req.query.nome
+    const qualquerNome = req.query.anime
+    const page = req.query.page
+    const limit = req.query.limit
+    
+    res.status(200).json({message: nome, anime: qualquerNome, pagina: page, limite: limit})
+});
+
+app.get ("/:nome/:sobrenome/:idade/:genero/:anime", (req, res) => {
+    const nome = req.params.nome
+    const sobrenome = req.params.sobrenome
+    const idade = req.params.idade
+    const genero = req.params.genero
+    const anime = req.params.anime
+    res.status(200).json({message:`ola meu nome é ${nome} ${sobrenome}, tenho ${idade} anos, meu genero é ${genero}
+     e gosto do anime ${anime}` })
 });
 
 app.post("/", (req, res) =>{
-    res.status(200).json({message: "hello post"})
+    const nome = req.body.nome;
+    const sobrenome = req.body.sobrenome;
+    const idade = req.body.idade;
+    res.status(200).json({nomeUsuario: nome, sobrenomeUsuario: sobrenome, idadeUsuario: idade})
 })
 
 app.put("/", (req, res) =>{
@@ -19,7 +37,7 @@ app.put("/", (req, res) =>{
 })
 
 app.delete("/", (req, res) =>{
-    res.status(200).json({message: "hello dele"})
+    res.status(200).json({message: "hello delete"})
 })
 
 app.listen(port, () =>{
